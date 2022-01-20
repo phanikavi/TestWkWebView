545914d9d5113573d0e9ccd115662c51ca63d8c6
phanikavi <7852905+phanikavi@users.noreply.github.com>
2022-01-20 09:42:59 -0500

    Initial Commit

---
+++ TestWkWebView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestWkWebView
+//
+//  Created by Phani Yarlagadda on 1/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
